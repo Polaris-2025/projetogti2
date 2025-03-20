31e3fca9a681dcaa1b7c596a946e22a0be2c222b
Polaris-2025 <pollaressy@gmail.com>
2025-03-20 10:55:47 -0300

    Estudos sobre rotas

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,31 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/Identificação', function () {
+    return " A Eliminação realizada com sucesso";
+});
+
+
+Route::get('/Eliminados/{Ilunminados}', function ($Iluminados) {
+    return " Já elvis:  ".$Iluminados;
+});
+
+
+Route::get('/subtração/{num777}/{num780}', function ($num777,$num780) {
+    return " A subtração é:  ".$num777 - $num780;
+});
+
+oute::get('/multiplicação/{num777}/{num780}', function ($num777,$num780) {
+    return " A multiplicação é:  ".$num777 * $num780;
+});
+
+
+oute::get('/divisão/{num777}/{num780}', function ($num777,$num780) {
+    return " A divisão é:  ".$num777 / $num780;
+});
+
+
+
+
+
